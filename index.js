4d278a2f06490b000db409ba827d9b1e46cbf8d3
tong41 <tongtong.workmotion@gmail.com>
2024-01-06 22:04:19 +0700

    new 3

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const profile = require("./routes/profile");
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/profile", profile);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
