68c59014682567b8e943e430066c26f28d931d11
tong41 <tongtong.workmotion@gmail.com>
2024-01-06 22:28:36 +0700

    add skill

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const profile = require("./routes/profile");
+const skill = require('./routes/skill')
 
 app.use(express.json({ extended: false }));
 
 app.use("/api/profile", profile);
+app.use("/api/skill", skill);
 
-const PORT = 8000;
+
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
